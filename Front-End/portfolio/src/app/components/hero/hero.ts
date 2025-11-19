be48901d73277df74671d44e827a5eb409d8869b
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2025-11-19 15:29:23 +0200

    first Push

---
+++ Front-End/portfolio/src/app/components/hero/hero.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  imports: [],
+  standalone: true,
+  templateUrl: './hero.html',
+  styleUrl: './hero.css',
+})
+export class Hero {
+
+}
