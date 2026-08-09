fc9d35d927489df248ca32dede490df409b2ce81
Nkosinathi <78370347+Ghost-nae@users.noreply.github.com>
2026-08-09 23:35:41 +0200

    Add ContactMessage entity with fields and annotations

--- App/Backend/src/main/java/com/portfolio/my/Portfolio/Models/ContactMessage.java
+++ App/Backend/src/main/java/com/portfolio/my/Portfolio/Models/ContactMessage.java
@@ -1,1 +1,35 @@
 package com.portfolio.my.Portfolio.Models;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "Message")
+@Getter
+@Setter
+@AllArgsConstructor
+public class ContactMessage{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = false)
+  private String fullname;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false, unique = false)
+  private String subject;
+
+  @Column(nullable = false)
+  private String message;
+
+  private LocalDate createdAt;
+
+  public ContactMessage() {}
+  
+}
